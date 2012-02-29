5b215704e6a0fccf44c86532b6b691e451dc2f22
Davide Principi <davide.principi@nethesis.it>
2012-02-29 17:31:03 +0100

    Added posixAccount provisioning action.
    NethServer::Directory refactored as Net::LDAP subclass

--- root/usr/share/perl5/vendor_perl/NethServer/Directory.pm
+++ root/usr/share/perl5/vendor_perl/NethServer/Directory.pm
@@ -4,17 +4,77 @@ use strict;
 
 use Net::LDAP;
 use Authen::SASL;
+use esmith::ConfigDB;
+use esmith::util;
+
+use Data::Dumper;
+
+our @ISA = qw(Net::LDAP);
 
 #
 # Connection setup
 #
-sub bindLdap
+sub new 
 {
-    my $LDAPURL = shift || 'ldapi://';
-    my $Ldap = Net::LDAP->new($LDAPURL) or die("Can't connect $LDAPURL");
-    my $sasl = Authen::SASL->new(mechanism=>'EXTERNAL');   
-    $Ldap->bind(anonymous=>0, sasl => $sasl) or die ("Can't bind to $LDAPURL");
-    return $Ldap;
+    my $class = shift;
+    my $url = shift || 'ldapi://';
+    my $self = $class->SUPER::new($url, @_) or die("Can't connect $url");
+
+    if($url eq 'ldapi://') {
+	$self->bind;
+    }
+
+    $self->{ConfigDb} = esmith::ConfigDB->open_ro();
+
+    bless ($self, $class);
+    return $self;
+}
+
+sub bind
+{
+    my $self = shift;
+    my $sasl = Authen::SASL->new(mechanism=>'EXTERNAL');
+    return $self->SUPER::bind(anonymous=>0, sasl => $sasl) or die ("Can't bind to " . $self->uri());
+}
+
+
+sub getDomainSuffix
+{
+    my $self = shift;
+    my $DomainName = $self->{ConfigDb}->get_value('DomainName');
+    return esmith::util::ldapBase($DomainName);
+}
+
+sub getInternalSuffix
+{
+    return 'dc=directory,dc=nh';
+}
+
+#
+# Check if an entry exists then add or modify
+#
+sub merge
+{
+    my $self = shift;
+    my $arg = Net::LDAP::_dn_options(@_);
+    my $message;
+
+    my $searchResult = $self->search(
+	base => $arg->{dn},
+	filter => 'objectClass=*',
+	sizelimit => 1,
+	scope => 'base'
+	);
+   
+    if($searchResult->is_error && $searchResult->code() != Net::LDAP::Constant::LDAP_NO_SUCH_OBJECT) {
+	$message = $searchResult;
+    } elsif($searchResult->count() == 0) {
+	$message = $self->add($arg->{dn}, attrs => $arg->{merge});
+    } else {
+	$message = $self->modify($arg->{dn}, replace => $arg->{merge});
+    }
+
+    return $message;
 }
 
 1;
