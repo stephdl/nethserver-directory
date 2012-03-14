ae6437a2d6f5b2bff5cfcecf1b7f570530594b03
Davide Principi <davide.principi@nethesis.it>
2012-03-14 11:55:58 +0100

    LDAP.pm: improved merge logic.
    - scalar values are replaced
    - array values are concatenated and duplicate items are removed

--- root/usr/share/perl5/vendor_perl/NethServer/Directory/LDAP.pm
+++ root/usr/share/perl5/vendor_perl/NethServer/Directory/LDAP.pm
@@ -5,7 +5,7 @@ use strict;
 use Net::LDAP;
 use Authen::SASL;
 use esmith::ConfigDB;
-
+use List::MoreUtils qw(uniq);
 use Data::Dumper;
 
 our @ISA = qw(Net::LDAP);
@@ -17,8 +17,17 @@ sub new
 {
     my $class = shift;
     my $url = shift || 'ldapi://';
-    my $self = $class->SUPER::new($url, @_) or return undef;
+    my %options = @_;
+    $options{'async'} = 0;    
+    $options{'onerror'} = sub { 
+	my $result = shift;
+	warn join(' ', $result->error_name(), $result->error());
+	return $result;
+    };
 
+    my $self = $class->SUPER::new($url, %options) or return undef;
+
+    # Automatic-bind if url is ldapi://
     if($url eq 'ldapi://') {
 	$self->bind;
     }
@@ -55,7 +64,44 @@ sub merge
     } elsif($searchResult->count() == 0) {
 	$message = $self->add($arg->{dn}, attrs => $arg->{merge});
     } else {
-	$message = $self->modify($arg->{dn}, replace => $arg->{merge});
+	my $updateResult;
+	#
+	# loop on $searchResult, replace string values, append list values
+	#
+	foreach my $entry ($searchResult->entries()) {
+	    my %updateAttributes = @{$arg->{'merge'}};
+	    my %mergedAttributes = ();
+
+
+	    foreach my $key ( keys(%updateAttributes) ) {
+		# skip 'dn' key:
+		if($key eq 'dn') {
+		    next;
+		}
+			       
+		if(ref $updateAttributes{$key} eq 'SCALAR' || !$entry->exists($key)) {
+		    # Scalar values and new values (of any type) 
+		    # are fully replaced:
+		    $mergedAttributes{$key} = $updateAttributes{$key};
+		} elsif(ref $updateAttributes{$key} eq 'ARRAY') {
+		    # New array value is concatenated with the old value
+		    # removing duplicate items
+		    my %h = map { $_ => 1 } @{[($entry->get_value($key)), @{$updateAttributes{$key}}]};		    
+		    $mergedAttributes{$key} = [keys %h];
+		}
+	    }
+
+	    $entry->replace(%mergedAttributes);
+	    $updateResult = $entry->update($self);
+	    if($updateResult->is_error()) {
+		$message = $updateResult;
+	    }
+	}
+		
+	# If not defined at this point, set $message to the last $updateResult
+	if(! defined $message) {
+	    $message = $updateResult;
+	}
     }
 
     return $message;
