5676c60a113fe73abc1bac21a1ab59012e3a5111
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2014-01-17 18:25:23 +0100

    Dashboard: widget for accounts. Refs #1671

---
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Dashboard_SystemStatus_Accounts.php
@@ -0,0 +1,8 @@
+<?php
+
+$L['accounts_title'] = 'Accounts';
+$L['group_label'] = 'Groups';
+$L['user_label'] = 'Users';
+$L['pseudonym_label'] = 'Pseudonyms';
+$L['ibay_label'] = 'Shared folders';
+$L['machine_label'] = 'Machine accounts';
---
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Dashboard_SystemStatus_Accounts.php
@@ -0,0 +1,9 @@
+<?php
+
+$L['accounts_title'] = 'Account';
+$L['group_label'] = 'Grouppi';
+$L['user_label'] = 'Utenti';
+$L['pseudonym_label'] = 'Pseudonimi';
+$L['ibay_label'] = 'Cartelle condivise';
+$L['machine_label'] = 'Account macchine';
+
