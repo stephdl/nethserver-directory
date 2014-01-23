11ada1931093e63a658a69178acde622de7e9a78
Davide Principi <davide.principi@nethesis.it>
2014-01-23 11:54:49 +0100

    Fixed "pseudonym" translation. Refs #1671

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Dashboard_SystemStatus_Accounts.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Dashboard_SystemStatus_Accounts.php
@@ -3,6 +3,6 @@
 $L['accounts_title'] = 'Accounts';
 $L['group_label'] = 'Groups';
 $L['user_label'] = 'Users';
-$L['pseudonym_label'] = 'Pseudonyms';
+$L['pseudonym_label'] = 'Email addresses';
 $L['ibay_label'] = 'Shared folders';
 $L['machine_label'] = 'Machine accounts';
--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Dashboard_SystemStatus_Accounts.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Dashboard_SystemStatus_Accounts.php
@@ -3,7 +3,7 @@
 $L['accounts_title'] = 'Account';
 $L['group_label'] = 'Grouppi';
 $L['user_label'] = 'Utenti';
-$L['pseudonym_label'] = 'Pseudonimi';
+$L['pseudonym_label'] = 'Indirizzi email';
 $L['ibay_label'] = 'Cartelle condivise';
 $L['machine_label'] = 'Account macchine';
 
