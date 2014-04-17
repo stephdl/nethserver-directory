ddce8c0ac90fd4e28f1dca5f788cf84af3063c55
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2014-04-17 10:24:56 +0200

    Web UI: add missing translation. Refs #2706

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Dashboard_SystemStatus_Accounts.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Dashboard_SystemStatus_Accounts.php
@@ -6,3 +6,5 @@ $L['user_label'] = 'Users';
 $L['pseudonym_label'] = 'Email addresses';
 $L['ibay_label'] = 'Shared folders';
 $L['machine_label'] = 'Machine accounts';
+$L['ftp_label'] = 'FTP accounts';
+$L['vpn_label'] = 'VPN accounts';
--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Dashboard_SystemStatus_Accounts.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Dashboard_SystemStatus_Accounts.php
@@ -6,4 +6,5 @@ $L['user_label'] = 'Utenti';
 $L['pseudonym_label'] = 'Indirizzi email';
 $L['ibay_label'] = 'Cartelle condivise';
 $L['machine_label'] = 'Account macchine';
-
+$L['ftp_label'] = 'Account FTP';
+$L['vpn_label'] = 'Account VPN';
