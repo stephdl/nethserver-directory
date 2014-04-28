351aa584d8a6fac546428c5376183a4ec3327266
Victor Moreno <victor_moreno_rubio@hotmail.com>
2014-04-28 14:03:06 +0200

    Web interface: update Spanish translation. Refs #2732

---
+++ root/usr/share/nethesis/NethServer/Language/es/NethServer_Module_Dashboard_SystemStatus_Accounts.php
@@ -0,0 +1,10 @@
+<?php
+
+$L['accounts_title'] = 'Cuentas';
+$L['group_label'] = 'Grupos';
+$L['user_label'] = 'Usuarios';
+$L['pseudonym_label'] = 'Direcciones de Emails';
+$L['ibay_label'] = 'Carpetas compartidas';
+$L['machine_label'] = 'Cuentas del equipo';
+$L['ftp_label'] = 'Cuentas FTP';
+$L['vpn_label'] = 'Cuentas VPN';
---
+++ root/usr/share/nethesis/NethServer/Language/es/NethServer_Module_Group.php
@@ -0,0 +1,16 @@
+<?php 
+
+$L['Create a new group'] = 'Crear un nuevo grupo'; 
+$L['Description_label'] = 'Descripción'; 
+$L['GroupTab_Title'] = 'Grupo'; 
+$L['Group_Description'] = 'Administrar grupos de sistema'; 
+$L['Group_Tags'] = 'Correspondencia de los miembros del grupo'; 
+$L['Group_Title'] = 'Grupos'; 
+$L['Key_label'] = 'Nombre del grupo'; 
+$L['Members'] = 'Miembros'; 
+$L['Members_label'] = 'Miembros'; 
+$L['PlugService_Title'] = 'Servicios'; 
+$L['Update group `${0}`'] = 'Actualizar grupo `${0}`'; 
+$L['groupname_label'] = 'Nombre del grupo'; 
+$L['valid_platform,group-create,group-length,1'] = 'El nombre es demasiado largo: ${reason}';
+$L['valid_platform,group-create,uniq-account,3'] = 'El nombre del grupo `${1}` ya se encuentra registrado';
