2b187ee71299d0542271706cc0d3dfa8b105598c
Davide Principi <davide.principi@nethesis.it>
2015-05-18 14:16:38 +0200

    Show first tab when view is shown. Refs #2728

--- root/usr/share/nethesis/NethServer/Template/Group/Modify.php
+++ root/usr/share/nethesis/NethServer/Template/Group/Modify.php
@@ -1,4 +1,6 @@
 <?php
+/* @var $view \Nethgui\Renderer\Xhtml */
+
 $view->requireFlag($view::INSET_FORM);
 
 if ($view->getModule()->getIdentifier() == 'update') {
@@ -26,3 +28,12 @@ $tabs = $view->tabs()
 echo $tabs;
 
 echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_CANCEL | $view::BUTTON_HELP);
+
+$actionId = $view->getUniqueId();
+$view->includeJavascript("
+jQuery(function($){
+    $('#${actionId}').on('nethguishow', function () {
+        $(this).find('.Tabs').tabs('select', 0);
+    });
+});
+");
